4da492f0b9989fc88b0485a97f8a52e9495de3ed
Krzysztof Szachna-Krause <kszk@gmx.us>
2019-07-26 16:22:21 +0200

    Added chart

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+var signups = 0;
+var ftd = 0;
+var earned = 0;
 
 function mobileView() {
     var isHide = document.querySelector('#notStaticMenu').classList.contains('mobile-hide');
@@ -45,3 +48,137 @@ function removeClass(that) {
         that.classList.remove('btn-icon--bigger')
     }
 }
+
+function closeModal() {
+    document.getElementById('overlay').classList.remove('show')
+}
+
+document.querySelectorAll('#overlay .js--close-modal').forEach(function (btn) {
+    btn.addEventListener('click', function (e) {
+        e.preventDefault()
+        closeModal()
+    })
+})
+
+document.querySelector('#overlay').addEventListener('click', function (e) {
+    if (e.target === this) {
+        closeModal()
+    }
+})
+
+document.addEventListener('keyup', function (e) {
+    if (e.keyCode === 27) {
+        closeModal()
+    }
+})
+
+function openModal(modal) {
+    if (screen.width > 767) {
+        document.querySelectorAll('#overlay > *').forEach(function (modal) {
+            modal.classList.remove('show')
+        })
+        document.querySelector('#overlay').classList.add('show')
+        document.querySelector(modal).classList.add('show')
+
+    }
+}
+
+function hideBar(whichOne) {
+    switch (whichOne) {
+        case 'signups':
+            signups === 1 ? signups = 0 : signups = 1;
+            break;
+
+        case 'ftd':
+            ftd === 1 ? ftd = 0 : ftd = 1;
+            break;
+
+        case 'earned':
+            earned === 1 ? earned = 0 : earned = 1;
+            break;
+    }
+
+    chart();
+
+}
+
+
+var ctx = document.getElementById('myChart').getContext('2d');
+
+chart();
+function chart() {
+    Chart.defaults.global.legend.display = false;
+    new Chart(ctx, {
+        // 1
+        type: 'bar',
+        data: {
+            // 2
+            labels: ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10"],
+            // 3
+            datasets: [{
+                // 4
+                label: "Signups",
+                // 5
+                backgroundColor: '#8DBEC8',
+                borderColor: '#8DBEC8',
+                // 6
+                data: [52, 51, 41, 94, 26, 6, 72, 9, 21, 88],
+                hidden: signups,
+            },
+            {
+                label: "FTD",
+                backgroundColor: '#F29E4E',
+                borderColor: '#F29E4E',
+                data: [6, 72, 1, 0, 47, 11, 50, 44, 63, 76],
+                hidden: ftd,
+            },
+            {
+                label: "Earned",
+                backgroundColor: '#71B374',
+                borderColor: '#71B374',
+                data: [59, 49, 68, 90, 67, 41, 13, 38, 48, 48],
+                // 7
+                hidden: earned,
+            }]
+        },
+
+    })
+};
+
+
+
+var ctxModal = document.getElementById('myChartModal').getContext('2d');
+Chart.defaults.global.legend.display = true;
+var chart2 = new Chart(ctxModal, {
+
+    // 1
+    type: 'bar',
+    data: {
+        // 2
+        labels: ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10"],
+        // 3
+        datasets: [{
+            // 4
+            label: "Signups",
+            // 5
+            backgroundColor: '#8DBEC8',
+            borderColor: '#8DBEC8',
+            // 6
+            data: [52, 51, 41, 94, 26, 6, 72, 9, 21, 88],
+        },
+        {
+            label: "FTD",
+            backgroundColor: '#F29E4E',
+            borderColor: '#F29E4E',
+            data: [6, 72, 1, 0, 47, 11, 50, 44, 63, 76],
+        },
+        {
+            label: "Earned",
+            backgroundColor: '#71B374',
+            borderColor: '#71B374',
+            data: [59, 49, 68, 90, 67, 41, 13, 38, 48, 48],
+            // 7
+
+        }]
+    },
+});
